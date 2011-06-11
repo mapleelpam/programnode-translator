61059476d10aa69c7bde493cb09f35684d5957c3
mapleelpam <mapleelpam@gmail.com>
2011-06-11 19:22:15 +0800

    refine typo error #17

--- src/pn-ld.cpp
+++ src/pn-ld.cpp
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
 		exit(1);
 	} catch (...) {
 		std::cout << "ERROR " << std::endl;
-		std::cout << "ERROR " << "Error while parsing zcc-flex options Exiting" << std::endl;
+		std::cout << "ERROR " << "Error while parsing pn-ld options Exiting" << std::endl;
 		exit(1);
 	}
 
