cbb9d2c3a437b13f7be7e04599ad5d1622757800
Danty <danty.tw@gmail.com>
2011-08-26 09:40:09 +0800

    add parameter for missed file insymbol #135

---
+++ include/as/symbol/parameter.h
@@ -0,0 +1,53 @@
+/* ***************************************************************
+ * Copyright 2011 Kai-Feng Chou - mapleellpam at gmail dot com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * ProgrameNode Translator
+ * Copyright 2011 mapleellpam at gmail dot com   All rights reserved.
+ *
+ * https://github.com/mapleelpam/programnode-translator
+ *
+ * Author: mapleelpam at gmail.com - Kai-Feng Chou - maple
+ * ***************************************************************/
+
+#ifndef __TW_MAPLE_AS_SYMBOL_PARAMTER_H_
+#define __TW_MAPLE_AS_SYMBOL_PARAMTER_H_
+
+#include <as/symbol/variable.h>
+
+namespace tw { namespace maple { namespace as { namespace symbol {
+struct Scope;
+struct Parameter : public Variable
+{
+	Parameter( std::string n, Scope *parent,  uint p = Symbol::T_NONE )
+		: Variable( n, parent, p )
+		, m_have_init( false )
+	{
+	}
+
+public:
+	void setHaveInit( bool b ) { m_have_init = b;	}
+	bool getHaveInit( ) { return m_have_init;	}
+private:
+	bool m_have_init;
+
+};
+
+typedef SHARED_PTR(Parameter) ParameterPtr;
+
+}}}}//tw/maple/as/symbol
+
+#endif
+
